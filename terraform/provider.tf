8f8da36f74ceb854f41a0cf19653f3ca857f9391
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-05-24 20:25:23 +0530

    Jenkins frontend deploy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
 backend "s3" {
-  bucket = "expense-infra-dev-remote-state"
+  bucket = "expense-infra-dev-remote-state-1"
   key = "expense-infra-dev-frontend"
   region = "us-east-1"
-  dynamodb_table="expense-infra-dev-remote-state-locking"
+  dynamodb_table="expense-infra-dev-locking-1"
   }
 }
 
