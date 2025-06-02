d836e4f42d1b6bd77617f52f65ae969b62051f1c
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-02 15:01:09 +0530

    jenkins frontend deploy

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -4,4 +4,4 @@ environment=$2
 app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.10.3.expense-ansible-roles-tf-jenkins-CD.git main-tf.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
+ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.14.expense-ansible-roles-tf-jenkins-cd.git main-tf.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
